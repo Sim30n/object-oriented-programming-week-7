3b046770c72b41d52e09316b5d9718d16d4bd11a
Sim30n <petteri.sarkka@gmail.com>
2020-03-04 13:34:50 +0200

    Third commit for the android studio exercises in object orientated programming course week 7. it contains exercises 7.1, 7.2 and 7.3.

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -2,24 +2,84 @@ package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context = null;
     TextView text;
+
+    //Button mButton;
+    EditText mEdit;
+    TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // this.testFunction();
+        context = MainActivity.this;
         text = (TextView) findViewById(R.id.textView);
+        System.out.println(context.getFilesDir());
     }
 
     public void testFunction(View v){
-        //System.out.println("Hello World!");
-        text.setText("Moikka maailma!");
+        text.setText("Hello World!");
+    }
+
+    public void tulostaSyote(View view) {
+        mEdit   = (EditText)findViewById(R.id.editText1);
+        mText = (TextView)findViewById(R.id.textView1);
+        mText.setText("Syötetty teksti: "+mEdit.getText().toString());
+    }
+
+
+    public void readFile(View v){
+        try{
+            InputStream ins = context.openFileInput("testi.txt"); //TODO Tälle arvo!
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(ins));
+            String s ="";
+
+            while((s=br.readLine()) != null){
+                System.out.println(s);
+            }
+            ins.close();
+        } catch(IOException e) {
+            Log.e("IOException", "Virhe syötteessä");
+        } finally {
+            System.out.println("Luettu");
+        }
+
+    }
+
+    public void writeFile(View v ){
+        try{
+            OutputStreamWriter ows = new OutputStreamWriter(context.openFileOutput("testi.txt", Context.MODE_PRIVATE));
+
+            String s = "";
+            s = "Tämä tulee tiedostoon \n Lue tiedosto jotta näet tämän \n tai sitten et näe" ;
+            ows.write(s);
+            ows.close();
+        } catch(IOException e) {
+            Log.e("IOException", "Virhe syötteessä");
+        } finally {
+            System.out.println("Kirjoitettu");
+        }
+
     }
 }
+
